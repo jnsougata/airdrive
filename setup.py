e65ff242a8ba28d5aa4f7403c51f834940dc271b
Zen <jnsougata@gmail.com>
2022-01-12 21:26:31 +0530

    reformat

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='airdrive',
-    version='0.0.1',
+    version='0.1.1',
     description='Unlimited cloud storage for your files',
     long_description=readme(),
     long_description_content_type="text/markdown",
